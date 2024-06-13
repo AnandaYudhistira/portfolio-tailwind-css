4d9b3240171a5efeafd896f74f1c75a06c288087
AnandaYudhistira <putuadnyana657@gmail.com>
2024-06-13 23:52:36 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        primary: '#0284c7',
+        secondary: '#0f172a'
+      },
+      screens: {
+        '2xl' : '1320px'
+      }
+    },
+  },
+  plugins: [],
+};
